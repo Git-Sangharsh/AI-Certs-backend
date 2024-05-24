bac49e1b4fbc9cf82333efb3e1bdd99e578b6c1a
sangharsh <sangharshdev69@gmail.com>
2024-05-25 01:07:39 +0530

    established mongodb Connection via mongoose

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 const port = 5000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ai-certs")
+  .then(() => {
+    console.log("mongoDb Connected");
+  })
+  .catch((err) => {
+    console.log("mongoDb Error: " + err);
+  });
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
